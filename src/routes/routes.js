5654eedc126a8a74818e7834497f6910707a7132
Gunjan Parmar <gunjan.parmar@carvana.com>
2021-12-30 10:41:25 -0500

    auth0 integration

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,9 +7,11 @@ const asyncWrap_1 = __importDefault(require("../utils/asyncWrap"));
 const health_controller_1 = __importDefault(require("../controllers/health.controller"));
 const ioc_container_1 = __importDefault(require("../ioc.container"));
 const user_controller_1 = __importDefault(require("../controllers/user.controller"));
+const authz_middleware_1 = require("../utils/authz.middleware");
 function default_1(application) {
     const healthController = ioc_container_1.default.get(health_controller_1.default);
     application.get('/healthStatus', (0, asyncWrap_1.default)(healthController.healthCheck.bind(healthController)));
+    application.use(authz_middleware_1.checkJwt);
     const userController = ioc_container_1.default.get(user_controller_1.default);
     application.get('/users', (0, asyncWrap_1.default)(userController.getUsers.bind(userController)));
     application.get('/users/:userId', (0, asyncWrap_1.default)(userController.getUser.bind(userController)));
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,13 +3,16 @@ import asyncWrap from "../utils/asyncWrap";
 import HealthController from "../controllers/health.controller";
 import container from "../ioc.container";
 import UserController from "../controllers/user.controller";
+import { checkJwt } from "../utils/authz.middleware";
 
 export default function (application: Application) {
     
     const healthController = container.get<HealthController>(HealthController);
     application.get('/healthStatus', asyncWrap(healthController.healthCheck.bind(healthController)));
 
-    const userController = container.get<UserController>(UserController);
+    // 🔐 Secure endpoints with Auth0 🔐 👇🏼
+    application.use(checkJwt);
+    const userController = container.get<UserController>(UserController); 
     application.get('/users', asyncWrap(userController.getUsers.bind(userController)));
     application.get('/users/:userId', asyncWrap(userController.getUser.bind(userController)));
 }
