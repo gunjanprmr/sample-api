3fbecfe423604129741c01fe45e2657522eb1e65
gunjanprmr <36042578+gunjanprmr@users.noreply.github.com>
2021-12-30 11:35:25 -0500

    Delete .jest directory

--- .jest/setEnvVars.js
+++
@@ -1,5 +0,0 @@
-process.env.DB_USER = 'DMPAdmin';
-process.env.DB_PASSWORD = 'edDLHHMRcaJc5y21WhNf';
-process.env.DB_HOST = 'carvana-dmp-dev.database.windows.net';
-process.env.DB_NAME = 'DealerMarketplace';
-process.env.DB_PORT = '1433';
